3938c35c0a7e7a9571d6077ce999c15cfb65ecd6
mblenkhorn <55706741+mblenkhorn@users.noreply.github.com>
2021-05-25 22:51:41 +0100

    Updated PlayerController
    Added functionality for controls and the about page.

--- COMP2007 Assignment/Assets/Scripts/MainMenu.cs
+++ COMP2007 Assignment/Assets/Scripts/MainMenu.cs
@@ -6,15 +6,79 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
 
+    public GameObject controlsMenu;
+    public GameObject aboutMenu;
+    public GameObject mainMenu;
+
+    public bool isControlMenu;
+    public bool isAboutMenu;
+    public bool isMainMenu;
+
+    public void Start()
+    {
+        isMainMenu = true;
+    }
+
+
+
     public void StartGame()
     {
         SceneManager.LoadScene("SampleScene");
+    }
+
+    public void OpenControlsMenu()
+    {
+        if(isMainMenu || isAboutMenu)
+        {
+            isMainMenu = false;
+            isAboutMenu = false;
+            isControlMenu = true;
+        }
+
+        if (isControlMenu)
+        {
+            controlsMenu.SetActive(true);
+            mainMenu.SetActive(false);
+        }
+
+    }
+
+    public void OpenAboutMenu()
+    {
+        if (isMainMenu || isControlMenu)
+        {
+            isMainMenu = false;
+            isControlMenu = false;
+            isAboutMenu = true;
+        }
+
+        if(isAboutMenu)
+        {
+            aboutMenu.SetActive(true);
+        }
     }
 
     public void ExitGame()
     {
         Debug.Log("The game has exited");
         Application.Quit();
+    }
+
+    public void GoBack()
+    {
+        if (isControlMenu)
+        {
+            isControlMenu = false;
+            controlsMenu.SetActive(false);
+            mainMenu.SetActive(true);
+
+        }
+        else if (isAboutMenu)
+        {
+            isAboutMenu = false;
+            aboutMenu.SetActive(false);
+            mainMenu.SetActive(true);
+        }
     }
     
 }
