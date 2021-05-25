b329de11f095d6c725ff4cf8a7824d0bf6f572e5
mblenkhorn <55706741+mblenkhorn@users.noreply.github.com>
2021-05-25 22:53:16 +0100

    Updated DiceBlock
    Updated DiceBlock to include adding points functionality

--- COMP2007 Assignment/Assets/Scripts/DiceBlock.cs
+++ COMP2007 Assignment/Assets/Scripts/DiceBlock.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DiceBlock : MonoBehaviour
 {
@@ -8,7 +9,17 @@ public class DiceBlock : MonoBehaviour
 
     public int score;
 
-    public ScoreManager manager;
+    public int maxBlocks = 5;
+
+
+    public Text scoreText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0;
+        scoreText.text = score.ToString();
+    }
 
     // Update is called once per frame
     void Update()
@@ -16,9 +27,24 @@ public class DiceBlock : MonoBehaviour
         transform.Rotate(Vector3.up * rotateSpeed * Time.deltaTime);
     }
 
+    
+    public void Collect()
+    {
+        AddScore(1);
+        gameObject.SetActive(false);
+    }
+
+    
+
+    public void AddScore(int pointsToAdd)
+    {
+        score += pointsToAdd;
+        Debug.Log(score);
+        scoreText.text = score.ToString();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
-            manager.AddScore(score);
-            Destroy(gameObject); 
+        Collect();
     }
 }
