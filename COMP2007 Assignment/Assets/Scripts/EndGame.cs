555835ceea1944a0805fd8f8176e79cce854750f
mblenkhorn <55706741+mblenkhorn@users.noreply.github.com>
2021-05-25 23:14:01 +0100

    Updated EndGame.cs
    Added in the yellowdiceblock so that game will end if all dice blocks of each colour is collected.

--- COMP2007 Assignment/Assets/Scripts/EndGame.cs
+++ COMP2007 Assignment/Assets/Scripts/EndGame.cs
@@ -7,12 +7,13 @@ public class EndGame : MonoBehaviour
     public DiceBlock redDiceBlock;
     public DiceBlock blueDiceBlock;
     public DiceBlock greenDiceBlock;
+    public DiceBlock yellowDiceBlock;
     public GameObject menuPanel;
 
     private void Update()
     {
         if(redDiceBlock.score == redDiceBlock.maxBlocks && blueDiceBlock.score == blueDiceBlock.maxBlocks
-            && greenDiceBlock.score == greenDiceBlock.maxBlocks)
+            && greenDiceBlock.score == greenDiceBlock.maxBlocks && yellowDiceBlock.score == yellowDiceBlock.maxBlocks)
         {
             Time.timeScale = 0f;
             menuPanel.SetActive(true);
